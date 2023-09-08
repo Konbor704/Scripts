0ba35b5da64012e027a053ddf508bc22d589a2ff
Konbor <konbor12@gmail.com>
2023-09-08 07:50:29 +0200

    start

---
+++ install_script.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+echo "Hello World!"
+
+sudo pacman -Syu
+
+sudo pacman -S fish firefox feh wal picom flameshot neovim git nemo kitty pavucontrol
+
+sudo pacman -S ttf-jetbrains-mono ttf-font-awesome ttf-nerd-fonts-symbols
+
+sudo pacman -S --needed base-devel git
+
+git clone https://aur.archlinux.org/yay.git
+
+cd yay
+
+makepkg -si
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+
+nvm install node
+
+nvm use latest
+
+git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+
+git clone https://github.com/Konbor704/Astronvim.git ~/.config/nvim
+
+git clone
+
+mkdir ~/Documents/Coding
+mkdir ~/Documents/Notes
